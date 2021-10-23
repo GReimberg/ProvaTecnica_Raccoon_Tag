cc007bc9ee587155059b9839c536d5f1f7d47125
GReimberg <81345442+GReimberg@users.noreply.github.com>
2021-10-23 14:02:58 -0300

    Create testEmail3.js

---
+++ testEmail3.js
@@ -0,0 +1,152 @@
+/* 1 - Soma total menor que 300 e valor total igual a 21.45; */
+const data_test1 = [{
+    'product_id': 'ES7112',
+    'product_name': 'Pacote de Post-it Fenix 50 unidades',
+    'product_price': '14.95',
+    'product_quantity': '5',
+    'product_url': 'http://www.foo.com/p/ES7112',
+    'product_category': 'office',
+    'product_brand': 'Fenix'
+}, {
+    'product_id': 'BR8810',
+    'product_name': 'Miniatura Yoshi Haras',
+    'product_price': '6.50',
+    'product_quantity': '2',
+    'product_url': 'http://www.foo.com/p/BR8810',
+    'product_category': 'others',
+    'product_brand': 'Haras'
+}];
+
+const result1 = 21.45;
+
+/* 2 - Soma com apenas um ítem e valor total igual a 650.00; */
+const data_test2 = [{
+    'product_id': 'IN2350',
+    'product_name': 'Teclado Mecânico Rozar K600',
+    'product_price': '650.00',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN2350',
+    'product_category': 'peripherals',
+    'product_brand': 'Rozar'
+}];
+
+const result2 = 650.00;
+
+/*3 - Soma igual a 4871.42;  */
+const data_test3 = [{
+    'product_id': 'IN2350',
+    'product_name': 'Teclado Mecânico Rozar K600',
+    'product_price': '650.00',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN2350',
+    'product_category': 'peripherals',
+    'product_brand': 'Rozar'
+}, {
+    'product_id': 'IN4566',
+    'product_name': 'Monitor Gamer LED GL UltraWide 25"',
+    'product_price': '1399.99',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN4566',
+    'product_category': 'monitor',
+    'product_brand': 'GL'
+}, {
+    'product_id': 'ES7112',
+    'product_name': 'Pacote de Post-it Fenix 50 unidades',
+    'product_price': '14.95',
+    'product_quantity': '5',
+    'product_url': 'http://www.foo.com/p/ES7112',
+    'product_category': 'office',
+    'product_brand': 'Fenix'
+}, {
+    'product_id': 'BR8810',
+    'product_name': 'Miniatura Yoshi Haras',
+    'product_price': '6.50',
+    'product_quantity': '2',
+    'product_url': 'http://www.foo.com/p/BR8810',
+    'product_category': 'others',
+    'product_brand': 'Haras'
+}, {
+    'product_id': 'IN4566',
+    'product_name': 'Monitor Gamer LED GL UltraWide 25"',
+    'product_price': '1399.99',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN4566',
+    'product_category': 'monitor',
+    'product_brand': 'GL'
+}, {
+    'product_id': 'IN4566',
+    'product_name': 'Monitor Gamer LED GL UltraWide 25"',
+    'product_price': '1399.99',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN4566',
+    'product_category': 'monitor',
+    'product_brand': 'GL'
+}];
+
+const result3 = 4871.42;
+
+/* 4 - Soma igual a 2721.44; */
+const data_test4 = [{
+    'product_id': 'IN2350',
+    'product_name': 'Teclado Mecânico Rozar K600',
+    'product_price': '650.00',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN2350',
+    'product_category': 'peripherals',
+    'product_brand': 'Rozar'
+}, {
+    'product_id': 'IN4566',
+    'product_name': 'Monitor Gamer LED GL UltraWide 25"',
+    'product_price': '1399.99',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN4566',
+    'product_category': 'monitor',
+    'product_brand': 'GL'
+}, {
+    'product_id': 'ES7112',
+    'product_name': 'Pacote de Post-it Fenix 50 unidades',
+    'product_price': '14.95',
+    'product_quantity': '5',
+    'product_url': 'http://www.foo.com/p/ES7112',
+    'product_category': 'office',
+    'product_brand': 'Fenix'
+}, {
+    'product_id': 'BR8810',
+    'product_name': 'Miniatura Yoshi Haras',
+    'product_price': '6.50',
+    'product_quantity': '2',
+    'product_url': 'http://www.foo.com/p/BR8810',
+    'product_category': 'others',
+    'product_brand': 'Haras'
+}, {
+    'product_id': 'IN2350',
+    'product_name': 'Teclado Mecânico Rozar K600',
+    'product_price': '650.00',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN2350',
+    'product_category': 'peripherals',
+    'product_brand': 'Rozar'
+}];
+
+const result4 = 2721.44;
+
+/* 5 - Soma igual a 2049.99; */
+const data_test5 = [{
+    'product_id': 'IN2350',
+    'product_name': 'Teclado Mecânico Rozar K600',
+    'product_price': '650.00',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN2350',
+    'product_category': 'peripherals',
+    'product_brand': 'Rozar'
+}, {
+    'product_id': 'IN4566',
+    'product_name': 'Monitor Gamer LED GL UltraWide 25"',
+    'product_price': '1399.99',
+    'product_quantity': '1',
+    'product_url': 'http://www.foo.com/p/IN4566',
+    'product_category': 'monitor',
+    'product_brand': 'GL'
+}];
+
+const result5 = 2049.99;
