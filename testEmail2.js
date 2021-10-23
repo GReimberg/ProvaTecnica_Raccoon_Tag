6205d0d006dfb3d1b823fa48ce83de2bfc6710c8
GReimberg <81345442+GReimberg@users.noreply.github.com>
2021-10-23 13:58:17 -0300

    Create testEmail2.js

---
+++ testEmail2.js
@@ -0,0 +1,37 @@
+const data = [
+  /* 1 - Sem espaço entre R$ e número; */
+    {
+      "id": "35",
+      "name": "Miniatura Homem de Ferro",
+      "price": "R$50.00"
+    },
+    /* 2 - 1 espaço entre R$ e número; */
+    {
+      "id": "30",
+      "name": "Miniatura Magneto",
+      "price": "R$ 57.00"
+    },
+    /* 3 - Vários espaços entre R$ e número; */
+    {
+      "id": "25",
+      "name": "Miniatura Super Homem",
+      "price": "R$            37.00"
+    },
+    /* 4 - Sem R$; */
+    {
+      "id": "71",
+      "name": "Miniatura Bernard",
+      "price": "71.00"
+    },
+    /* 5 - Vírgula como marcador de decimal; */
+    {
+      "id": "56",
+      "name": "Miniatura Batman",
+      "price": "R$ 23,00"
+    },
+    /* 6 - Outra string no lugar do R$; */
+    {
+      "id": "56",
+      "name": "Miniatura Darth Vader",
+      "price": "Raccoon 89.00"
+    }];
